8024b13b071dd4bfe4d5cf6038d6fd59fe8302bb
Code With Dary <63154066+codewithdary@users.noreply.github.com>
2021-11-19 18:06:13 +0100

    Update show.blade.php

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -23,13 +23,17 @@
             <p class="font-bold text-l text-black pb-8">
                 Shipping: <span class="text-red-500">$ 25</span>
             </p>
+            
+            <p class="font-thin text-s text-black pb-8">
+                Apple M1 Pro, 16 GPU, 16GB, 512 GB SSD
+            </p>            
 
-            <p class="text-gray-800 text-thin text-l leading-6 pb-8">
+            <p class="text-gray-800 text-thin text-l leading-6 pb-12">
                 Apple MacBook Pro is a macOS laptop with a 13.30-inch display that has a resolution of 2560x1600 pixels. The Apple MacBook Pro packs 512GB of SSD storage. Connectivity options include Wi-Fi 802.11 ac, Bluetooth and it comes with 2 USB ports (2 x USB 3.0), Mic In ports.
             </p>
 
             <a 
-                class="btn btn-primary btn-active mb-10 w-full" 
+                class="px-10 py-6 text-l uppercase text-white font-bold bg-blue-600 rounded-full w-full" 
                 role="button" 
                 aria-pressed="true">
                 Add To Cart
